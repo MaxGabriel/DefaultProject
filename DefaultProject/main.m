fd8f3bc38f10a2d0fd0066fe2b564781ff313c2f
Maximilian Tagher <feedback.tagher@gmail.com>
2013-12-30 18:00:32 -0500

    Initial Commit

---
+++ DefaultProject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DefaultProject
+//
+//  Created by Maximilian Tagher on 12/30/13.
+//  Copyright (c) 2013 Tagher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
